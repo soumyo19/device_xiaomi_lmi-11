0baf322fa3dc851d1b467a2163a8bc7ef11b294e
dataoutputstream <mb150996@gmail.com>
2020-10-05 09:40:25 +0200

    Define audio configs and use tinymix for now

--- device.mk
+++ device.mk
@@ -27,7 +27,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/xiaomi/lmi/lmi-vendor.mk)
 
 
-
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    libaacwrapper \
+    tinymix
      
 # Camera
 PRODUCT_PACKAGES += \
