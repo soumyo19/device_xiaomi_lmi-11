eda9b783276e12e871f426d360c38dc597290d46
AbdullahPS <abdullahtabbal@github.com>
2021-01-01 20:03:38 +0000

    lmi:aex:add aex make files

---
+++ aosp_lmi.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+#Inherit from aex common
+$(call inherit-product, vendor/aosp/common.mk)
+# Inherit from lmi device
+$(call inherit-product, device/xiaomi/lmi/device.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_lmi
+PRODUCT_DEVICE := lmi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := POCO F2 PRO
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
