ad1f78c34bb874fb722c726476a103bdde1a7348
AbdullahPS <abdullahtabbal@github.com>
2021-01-01 13:17:40 +0000

    lmi:add initial overlays

--- device.mk
+++ device.mk
@@ -73,6 +73,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-aosp
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.ims.xml \
