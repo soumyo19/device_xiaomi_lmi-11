ea0e6808b8cfab9f3942147bff2325acfb00f810
AbdullahPS <abdullahtabbal@github.com>
2021-01-15 10:43:46 +0000

    lmi:Octavi-fy

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lmi.mk
+    $(LOCAL_DIR)/octavi_lmi.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lmi-user \
-    aosp_lmi-userdebug \
-    aosp_lmi-eng
+    octavi_lmi-user \
+    octavi_lmi-userdebug \
+    octavi_lmi-eng
--- octavi_lmi.mk
+++ octavi_lmi.mk
@@ -4,14 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 #Inherit from aex common
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 # Inherit from lmi device
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_lmi
+PRODUCT_NAME := octavi_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
@@ -19,3 +19,4 @@ PRODUCT_MODEL := POCO F2 PRO
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+$(call inherit-product, vendor/gapps/gapps.mk)
