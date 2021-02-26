9ffa9527791fb216c34d0daeb363615a86fcaa10
AbdullahPS <abdullahtabbal@github.com>
2021-02-26 14:01:00 +0200

    lmi:Fod:Adapt Fod Implementation work to havoc For some reason, Fod works just with these changes on havoc, faster and more accurate then the older one plus ScreenOff Fod works here. Lets use it!
    * Soumyo: Adapt FOD Implementation to work on Shapeshift as well.

--- device.mk
+++ device.mk
@@ -106,7 +106,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 
 PRODUCT_PACKAGES += \
-    lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_kona
+    lineage.biometrics.fingerprint.inscreen@1.0-service.kona
 
     
 # HIDL
