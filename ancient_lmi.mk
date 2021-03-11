a15a1e92332d65b749e2ae51ed5878804ebe7f3e
soumyo19 <fsoummya@gmail.com>
2021-03-11 18:36:11 +0000

    Lmi: Build with Gapps.

--- ancient_lmi.mk
+++ ancient_lmi.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/xiaomi/lmi/device.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
+# Ancient Identifier.
+ANCIENT_GAPPS := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := ancient_lmi
 PRODUCT_DEVICE := lmi
